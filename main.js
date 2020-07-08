e16f0314d777656197bc53dd87fd21c29af8ea68
KBG <kbg5213@gmail.com>
2020-07-08 11:26:06 +0900

    home opacity, add arrow

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
 'use strict'
 
-
 const navbar=document.querySelector('#navbar')
 const navbarHeight=navbar.getBoundingClientRect().height
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll',()=>{
-
 console.log(window.scrollY)
 console.log(navbarHeight)
-
-
 if(window.scrollY>=navbarHeight){
     navbar.classList.add('navbar--dark')
     const navbarDark=document.querySelector('#navbar.navbar--dark');
-    
+    home.style.opacity=1-(window.scrollY**1.4/6500);
     // navbarDark.style.opacity=window.scrollY/110
-
 }else{
     navbar.classList.remove('navbar--dark')
+    home.style.opacity=1;
+}
+if(window.scrollY>=homeHeight/2){
+    arrowUp.classList.add('visible');
+}else{
+    arrowUp.classList.remove('visible');
 }
 });
 
+arrowUp.addEventListener('click',(event)=>{
+    const home=document.querySelector('#home');
+    home.scrollIntoView({behavior: "smooth", block: "start"});
+
+});
 
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event)=>{
@@ -28,7 +37,16 @@ navbarMenu.addEventListener('click',(event)=>{
     const link = target.dataset.link;
     if(link!=null){
         console.log(link);
-
+        const menu=document.querySelector(link);
+        menu.scrollIntoView({behavior: "smooth", block: "start"});      
     }
 
-})
+});
+
+const contactButton = document.querySelector('.home__contact');
+contactButton.addEventListener('click',function(event){
+    const contact=document.querySelector('#contact');
+    contact.scrollIntoView({behavior: "smooth", block: "start"});
+
+});
+
