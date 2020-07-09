a972bbe13561e29b24ff3dd93b2ffe2307076592
KBG <kbg5213@gmail.com>
2020-07-09 15:11:08 +0900

    my work buttons event bind and project animation

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ console.log(navbarHeight)
 if(window.scrollY>=navbarHeight){
     navbar.classList.add('navbar--dark')
     const navbarDark=document.querySelector('#navbar.navbar--dark');
-    home.style.opacity=1-(window.scrollY**1.4/6500);
+    home.style.opacity=1-(window.scrollY**1.2/1500);
     // navbarDark.style.opacity=window.scrollY/110
 }else{
     navbar.classList.remove('navbar--dark')
@@ -50,3 +50,45 @@ contactButton.addEventListener('click',function(event){
 
 });
 
+// Projects
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll('.project');
+const btns = document.querySelectorAll(".category__btn");
+
+workBtnContainer.addEventListener('click',(event)=>{
+    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter
+    if(filter ==null){
+        return;
+    }
+    const target = event.target.classList.contains("category__btn")? event.target : event.target.parentNode;
+    btns.forEach((btn)=>{
+        if(btn==target){
+            btn.classList.add('active');
+        }else{
+            btn.classList.remove('active');
+        }   
+    })
+    
+
+
+
+
+    projectContainer.classList.add('anim-out');
+
+    setTimeout(()=>{
+        projects.forEach((project)=>{
+            // console.log(project.dataset.type);
+            // console.log(project.dataset.type!==filter)
+            if(filter ==='*' || project.dataset.type.split(", ").includes(filter)){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        });
+    projectContainer.classList.remove('anim-out');   
+    },300)
+    
+    
+})
